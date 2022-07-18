58eb7b669cef7bfcb2c5a16cd76551fe7e39a42a
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2022-07-18 18:22:40 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/teru01/gprogen
+
+go 1.18
+
+require (
+	github.com/mattn/go-tty v0.0.4
+	github.com/urfave/cli/v2 v2.11.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/mattn/go-tty"
+	"github.com/urfave/cli/v2"
+)
+
+var quitChan = make(chan struct{})
+
+func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "host",
+				Aliases: []string{"H"},
+				Value:   "0.0.0.0",
+				Usage:   "listening host",
+			},
+			&cli.Int64Flag{
+				Name:    "port",
+				Aliases: []string{"p"},
+				Value:   8080,
+				Usage:   "listening port",
+			},
+		},
+		Name:   "gprogen",
+		Usage:  "Go proxy and stub generator for load test",
+		Action: Start,
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+}
+
+func Start(c *cli.Context) error {
+	svc := http.Server{
+		Addr: c.String("host") + ":" + c.String("port"),
+	}
+
+	t, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer t.Close()
+	go func(t *tty.TTY) {
+		for {
+			r, err := t.ReadRune()
+			if err != nil {
+				log.Println(err)
+			}
+			if r == 'q' {
+				if err := svc.Shutdown(context.Background()); err != nil {
+					log.Println(err)
+				}
+				close(quitChan)
+				break
+			}
+			log.Printf("pressed: %v, Press q to quit.\n", string(r))
+		}
+	}(t)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	log.Println("listening on", svc.Addr)
+	if err := svc.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+	<-quitChan
+	return nil
+}
