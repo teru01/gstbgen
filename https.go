39a0c1ecb222947d5503077cd7d5a87d0b17e9da
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2022-09-04 18:28:19 +0900

    wip: create cert

--- https.go
+++ https.go
@@ -1,26 +1,95 @@
 package main
 
 import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/rand"
 	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
 	"io"
+	"math/big"
 	"os"
 
 	"github.com/elazarl/goproxy"
 	"github.com/urfave/cli/v2"
 )
 
-func createCertificate(certificateReader, keyReader io.Reader) (tls.Certificate, error) {
+func createCertificate(rootCertificateReader, rootKeyReader io.Reader) (tls.Certificate, error) {
 	var c tls.Certificate
-	cert, err := io.ReadAll(certificateReader)
+	rootCertB, err := io.ReadAll(rootCertificateReader)
 	if err != nil {
-		return c, fmt.Errorf("failed to read cert: %w", err)
+		return c, fmt.Errorf(": %w", err)
 	}
-	k, err := io.ReadAll(keyReader)
+	rootCertBlock, _ := pem.Decode(rootCertB)
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+	rootCertificate, err := x509.ParseCertificate(rootCertBlock.Bytes)
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+	k, err := io.ReadAll(rootKeyReader)
 	if err != nil {
 		return c, fmt.Errorf("failed to read key: %w", err)
 	}
-	return tls.X509KeyPair(cert, k)
+	rootKBlock, _ := pem.Decode(k)
+	rootKey, err := x509.ParsePKCS1PrivateKey(rootKBlock.Bytes)
+	if err != nil {
+		return c, fmt.Errorf("failed to read key: %w", err)
+	}
+
+	serial, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+	template := x509.Certificate{
+		SerialNumber: serial,
+		Issuer: pkix.Name{
+			Country:      []string{"JP", "US"},
+			Organization: []string{"gstbgenCA"},
+			CommonName:   "gstbgenCA",
+		},
+		Subject: pkix.Name{
+			Country:      []string{"JP", "US"},
+			Organization: []string{"gstbgen"},
+			CommonName:   "gstbgen",
+		},
+		AuthorityKeyId: rootCertificate.SubjectKeyId,
+	}
+
+	pub, priv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+	certB, err := x509.CreateCertificate(rand.Reader, &template, rootCertificate, pub, rootKey)
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+	var certPem, keyPem bytes.Buffer
+	if err := pem.Encode(&certPem, &pem.Block{Type: "CERTIFICATE", Bytes: certB}); err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+
+	marshaledKey, err := x509.MarshalPKCS8PrivateKey(priv)
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+	if err := pem.Encode(&keyPem, &pem.Block{Type: "PRIVATE KEY", Bytes: marshaledKey}); err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+
+	certPemB, err := io.ReadAll(&certPem)
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+	keyPemB, err := io.ReadAll(&keyPem)
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+	return tls.X509KeyPair(certPemB, keyPemB)
 }
 
 func enableHttpsProxy(c *cli.Context, proxy *goproxy.ProxyHttpServer) error {
