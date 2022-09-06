fc861cc0d4a95119f154a8f984d060916821baf0
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2022-09-06 23:47:28 +0900

    wip

--- https.go
+++ https.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha1"
 	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
@@ -17,6 +18,7 @@ import (
 
 	"github.com/elazarl/goproxy"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/net/idna"
 )
 
 func createRootCAInfo(rootCertificateReader, rootKeyReader io.Reader) (*x509.Certificate, *rsa.PrivateKey, error) {
@@ -44,41 +46,99 @@ func createRootCAInfo(rootCertificateReader, rootKeyReader io.Reader) (*x509.Cer
 	return rootCertificate, rootKey, nil
 }
 
-func createCertificate(host string, rootCertificate *x509.Certificate, rootKey *rsa.PrivateKey) (tls.Certificate, error) {
-	var c tls.Certificate
-	serial, err := rand.Int(rand.Reader, big.NewInt(1000000))
+func createIntermediateCertificate(rootCertificate *x509.Certificate, rootKey *rsa.PrivateKey) (*x509.Certificate, *rsa.PrivateKey, error) {
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serial, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
-		return c, fmt.Errorf(": %w", err)
+		return nil, nil, fmt.Errorf(": %w", err)
 	}
 
 	notBefore := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
 	notAfter := time.Now().AddDate(2, 0, 0)
-	cnHosts := strings.Split(host, ":")
+
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, fmt.Errorf(": %w", err)
+	}
+
 	template := x509.Certificate{
 		SerialNumber: serial,
 		NotBefore:    notBefore,
 		NotAfter:     notAfter,
 		Issuer: pkix.Name{
-			Country:      []string{"JP", "US"},
-			Organization: []string{"gstbgenCA"},
-			CommonName:   "gstbgenCA",
+			Country:            []string{"JP", "US"},
+			Organization:       []string{"mitmproxy"},
+			OrganizationalUnit: []string{"mitmproxy"},
+			CommonName:         "mitmproxy",
 		},
 		Subject: pkix.Name{
 			Country:      []string{"JP", "US"},
-			Organization: []string{"gstbgen"},
-			CommonName:   cnHosts[0],
+			Organization: []string{"gstbgenMidCA"},
 		},
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
 		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
 		BasicConstraintsValid: true,
-		IsCA:                  false,
+		IsCA:                  true,
 		AuthorityKeyId:        rootCertificate.SubjectKeyId,
+		SubjectKeyId:          bigIntHash(priv.N),
 	}
+
+	certB, err := x509.CreateCertificate(rand.Reader, &template, rootCertificate, &priv.PublicKey, rootKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf(": %w", err)
+	}
+
+	cert, err := x509.ParseCertificate(certB)
+	if err != nil {
+		return nil, nil, fmt.Errorf(": %w", err)
+	}
+	return cert, priv, nil
+}
+
+func createCertificate(host string, rootCertificate *x509.Certificate, rootKey *rsa.PrivateKey) (tls.Certificate, error) {
+	var c tls.Certificate
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serial, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return c, fmt.Errorf(": %w", err)
+	}
+
+	notBefore := time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	notAfter := time.Now().AddDate(2, 0, 0)
+
+	cnHosts := strings.Split(host, ":")
+	hostName := cnHosts[0]
+
+	hostName, err = idna.ToASCII(hostName)
+	if err != nil {
+		return c, err
+	}
+	hostName = strings.ToLower(hostName)
 
 	priv, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		return c, fmt.Errorf(": %w", err)
 	}
+
+	template := x509.Certificate{
+		SerialNumber: serial,
+		NotBefore:    notBefore,
+		NotAfter:     notAfter,
+		Issuer: pkix.Name{
+			Organization:       []string{"mitmproxy"},
+			OrganizationalUnit: []string{"mitmproxy"},
+			CommonName:         "mitmproxy",
+		},
+		Subject: pkix.Name{
+			CommonName: "*." + hostName,
+		},
+		ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		KeyUsage:       x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		AuthorityKeyId: rootCertificate.SubjectKeyId,
+		DNSNames:       []string{hostName, "*." + hostName},
+		SubjectKeyId:   bigIntHash(priv.N),
+	}
+
 	certB, err := x509.CreateCertificate(rand.Reader, &template, rootCertificate, &priv.PublicKey, rootKey)
 	if err != nil {
 		return c, fmt.Errorf(": %w", err)
@@ -88,11 +148,11 @@ func createCertificate(host string, rootCertificate *x509.Certificate, rootKey *
 		return c, fmt.Errorf(": %w", err)
 	}
 
-	marshaledKey, err := x509.MarshalPKCS8PrivateKey(priv)
-	if err != nil {
-		return c, fmt.Errorf(": %w", err)
-	}
-	if err := pem.Encode(&keyPem, &pem.Block{Type: "PRIVATE KEY", Bytes: marshaledKey}); err != nil {
+	marshaledKey := x509.MarshalPKCS1PrivateKey(priv)
+	// if err != nil {
+	// 	return c, fmt.Errorf(": %w", err)
+	// }
+	if err := pem.Encode(&keyPem, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: marshaledKey}); err != nil {
 		return c, fmt.Errorf(": %w", err)
 	}
 
@@ -105,6 +165,7 @@ func createCertificate(host string, rootCertificate *x509.Certificate, rootKey *
 		return c, fmt.Errorf(": %w", err)
 	}
 	fmt.Println(string(certPemB))
+	fmt.Println(string(keyPemB))
 	return tls.X509KeyPair(certPemB, keyPemB)
 }
 
@@ -119,22 +180,54 @@ func enableHttpsProxy(c *cli.Context, proxy *goproxy.ProxyHttpServer) error {
 	}
 	defer keyFile.Close()
 	defer certFile.Close()
+
+	// rootCA, err := tls.LoadX509KeyPair(c.String("cert"), c.String("key"))
+	// if err != nil {
+	// 	return fmt.Errorf(": %w", err)
+	// }
+
 	rootCert, rootKey, err := createRootCAInfo(certFile, keyFile)
 	if err != nil {
 		return fmt.Errorf("failed to create rootca info: %w", err)
 	}
+
+	// if rootCA.Leaf, err = x509.ParseCertificate(rootCA.Certificate[0]); err != nil {
+	// 	return err
+	// }
+
+	// cert, key, err := createIntermediateCertificate(rootCert, rootKey)
+	// if err != nil {
+	// 	return fmt.Errorf("failed to create rootca info: %w", err)
+	// }
+
 	httpsHandler := func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+		// certificate, err := createCertificate(host, cert, key)
 		certificate, err := createCertificate(host, rootCert, rootKey)
+		if err != nil {
+			fmt.Println(err)
+			return nil, ""
+		}
+
 		if err != nil {
 			fmt.Println(err)
 			// return fmt.Errorf("failed to create certificate: %w", err)
 		}
 		customConnectAction := &goproxy.ConnectAction{
-			Action:    goproxy.ConnectMitm,
-			TLSConfig: goproxy.TLSConfigFromCA(&certificate),
+			Action: goproxy.ConnectMitm,
+			TLSConfig: func(host string, ctx *goproxy.ProxyCtx) (*tls.Config, error) {
+				var config tls.Config
+				config.Certificates = append(config.Certificates, certificate)
+				return &config, nil
+			},
 		}
 		return customConnectAction, host
 	}
 	proxy.OnRequest().HandleConnect(goproxy.FuncHttpsHandler(httpsHandler))
 	return nil
+}
+
+func bigIntHash(n *big.Int) []byte {
+	h := sha1.New()
+	h.Write(n.Bytes())
+	return h.Sum(nil)
 }
